7dfc31d3a91faec7d83354cd9875147a9d83e17c
Zach Spangler <117198114+zspangler10@users.noreply.github.com>
2024-01-28 17:24:45 -0500

    Updated analytics scripts

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -12,7 +12,7 @@ import {
     Link,
     Button
 } from '@chakra-ui/react';
-import Fade from 'react-reveal/Fade';
+import {Fade} from 'react-reveal/Fade';
 import Card from '../generics/Card';
 
 interface ProjectCardProps {
